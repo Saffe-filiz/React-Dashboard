824636e70a85bdb69d4bb28e9c4316d41c676285
Saffe-filiz <saffetfiliz1989@gmail.com>
2022-05-20 09:03:06 +0300

    Pie Chart Style

--- src/Components/ThePieChart.js
+++ src/Components/ThePieChart.js
@@ -15,13 +15,15 @@ function PieCharts () {
 		<div className="pieChartContent">
 			<h2>Test</h2>
 			<div className="pieChart">
-				<PieChart width={285} height={285}>
-                    <Pie  data={data} cx={120} cy={200} innerRadius={60} outerRadius={80} fill="#8884d8" paddingAngle={5} dataKey="value" label>
+			<ResponsiveContainer width="100%" height="100%">
+				<PieChart width={800} height={400}>
+                    <Pie  data={data} cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#8884d8" paddingAngle={5} dataKey="value" label>
                         {data.map((entry, index) => (
                             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                         ))}
                     </Pie>
                 </PieChart>
+                </ResponsiveContainer>
 			</div>
 		</div>
 	)
