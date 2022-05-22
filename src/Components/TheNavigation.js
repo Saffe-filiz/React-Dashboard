8677c7c10bf1d343c9f4644768a71c5a56d92887
Saffe-filiz <saffetfiliz1989@gmail.com>
2022-05-22 14:19:11 +0300

    Navigation Style

--- src/Components/TheNavigation.js
+++ src/Components/TheNavigation.js
@@ -1,25 +1,26 @@
-
-import Dashboard from '../Assets/Icons/Dashboard.svg';
-import Orders from '../Assets/Icons/Orders.svg';
-import Account from '../Assets/Icons/Account.svg';
-import Setting from '../Assets/Icons/Setting.svg';
-import Chat from '../Assets/Icons/Chat.svg';
-import FAQ from '../Assets/Icons/FAQ.svg';
+import { ReactComponent as Dashboard} from '../Assets/Icons/Dashboard.svg';
+import { ReactComponent as Orders} from '../Assets/Icons/Orders.svg';
+import { ReactComponent as Account} from  '../Assets/Icons/Account.svg';
+import { ReactComponent as Setting} from '../Assets/Icons/Setting.svg';
+import { ReactComponent as Chat} from  '../Assets/Icons/Chat.svg';
+import { ReactComponent as FAQ} from  '../Assets/Icons/FAQ.svg';
 
 function Navigation () {
 	return (
 	<div className="navigation">
 		<nav>
 			<ul>
-				<li><img src={Dashboard}/><a href="">Dashboard</a></li>
-				<li><img src={Orders}/><a href="">Ordars</a></li>
-				<li><img src={Account}/><a href="">Account</a></li>
-				<li><img src={Setting}/><a href="">Setting</a></li>
+				<li><Dashboard/> <a href="">Dashboard</a></li>
+				<li><Orders/><a href="">Ordars</a></li>
+				<li><Account/><a href="">Account</a></li>
+				<li><Setting/><a href="">Setting</a></li>
 			</ul>
 			<div className="support">
 				<h3>Support</h3>
-				<a href=""><img src={Chat}/> Chat</a>
-				<a href=""><img src={FAQ}/> FAQ</a>
+				<ul>
+					<li><Chat/> <a href="">Chat</a></li>
+				    <li><FAQ/><a href="">FAQ</a></li>
+				</ul>
 			</div>
 		</nav>
 	</div>
